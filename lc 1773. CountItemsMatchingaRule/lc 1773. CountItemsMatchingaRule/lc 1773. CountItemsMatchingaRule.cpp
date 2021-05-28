50505d8eccc44589cc35804a4f5dfd21a7a5a982
SergeyCpp <golovachlena40@gmail.com>
2021-05-28 18:34:35 +0300

    leet

---
+++ lc 1773. CountItemsMatchingaRule/lc 1773. CountItemsMatchingaRule/lc 1773. CountItemsMatchingaRule.cpp
@@ -0,0 +1,66 @@
+﻿// lc1773. CountItemsMatchingaRule.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+/*
+You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+The ith item is said to match the rule if one of the following is true:
+
+ruleKey == "type" and ruleValue == typei.
+ruleKey == "color" and ruleValue == colori.
+ruleKey == "name" and ruleValue == namei.
+Return the number of items that match the given rule.
+
+Example 1:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+Output: 1
+Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+Example 2:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+Output: 2
+Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"].
+Note that the item ["computer","silver","phone"] does not match.
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int countMatches(vector<vector<string>>& vec, string ruleKey, string ruleValue)
+{
+    int x, sum = 0;
+
+    if (ruleKey == "type") x = 0;
+
+    else if (ruleKey == "color") x = 1;
+
+    else if (ruleKey == "name") x = 2;
+
+    else x = -1;
+
+
+    if (x != -1)
+    {
+        for (int i = 0; i < vec.size(); i++)
+        {
+            if (vec[i][x] == ruleValue)
+                sum++;
+        }
+    }
+
+    return sum;
+}
+
+int main()
+{
+    vector<vector<string>> items = { {"phone", "blue", "pixel"}, {"computer", "silver", "lenovo"}, {"phone", "gold", "iphone"} };
+    string ruleKey = "color";
+    string ruleValue = "silver";
+
+
+    cout << (countMatches(items, ruleKey, ruleValue));
+    return 0;
+}
